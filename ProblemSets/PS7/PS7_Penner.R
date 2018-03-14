059668ab33e4c54ee12f48177b54f9067f7d92de
Alexandra-Penner <35508350+Alexandra-Penner@users.noreply.github.com>
2018-03-14 13:04:08 -0500

    PS7 fix R Script

--- ProblemSets/PS7/PS7_Penner.R
+++ ProblemSets/PS7/PS7_Penner.R
@@ -1,8 +1,50 @@
 # PS7
 # Alli Penner
 
-
+setwd("~/GitHub/DScourseS18")
 library(mice)
 library(MixedDataImpute)
 library(stargazer)
+library(VIM)
+
+wage <- read.csv("Modeling/wages.csv")
+
+# drop tenure and hgc missingness
+wage <- wage[ !is.na(wage$hgc),]
+wage <- wage[ !is.na(wage$tenure),]
+
+stargazer(wage)
+
+scattmatrixMiss(wage)
+
+# Regression with imputations
+
+# tenure^2
+wage$tenure2 <- wage$tenure^2
+# complete cases
+wagecomplete <- wage[complete.cases(wage),]
+CCreg <- lm(logwage~hgc+college+tenure+tenure2+age+married, wagecomplete)
+
+# mean impute
+wagemean <- wage
+wagemean$logwage[is.na(wagemean$logwage)] <- mean(wage$logwage, na.rm = T)
+meanreg <- lm(logwage~hgc+college+tenure+tenure2+age+married, wagemean)
+
+# linear regress impute
+wagelm <- wage
+wagelm$logwage[is.na(wagelm$logwage)] <- predict(CCreg, 
+                                                 wagelm[is.na(wagelm$logwage),])
+lmreg <- lm(logwage~hgc+college+tenure+tenure2+age+married, wagelm)
+
+#pmm  impute
+temp <- mice(wage,m=5,maxit=50,meth='pmm',seed=500)
+wagemice <- complete(temp,1)
+micereg <- lm(logwage~hgc+college+tenure+tenure2+age+married, wagemice)
+
+# table
+stargazer(CCreg, meanreg, lmreg, 
+          column.labels=c("Complete","Mean", "Linear"))
+
+
+
 
